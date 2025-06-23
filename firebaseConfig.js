0973036e70c2b880802bdb532bef805c15a1e8de
tigeronawire <oliver1.kjeldsen@gmail.com>
2025-06-23 15:06:34 -0400

    Add files via upload

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,1 +1,9 @@
-// Firebase config placeholder
+// firebaseConfig.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyBLjz0BYFYKWExgW0hk1D5mYzzbHpYKiiI",
+  authDomain: "menumuse-a420c.firebaseapp.com",
+  projectId: "menumuse-a420c",
+  storageBucket: "menumuse-a420c.firebasestorage.app",
+  messagingSenderId: "333441296439",
+  appId: "1:333441296439:web:e255b7e1b21969067037b2"
+};
